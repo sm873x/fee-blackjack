ae90fb09a5d2485eccc053b6a32adfa2319f5ca4
Stella Ma <sm873x@gmail.com>
2016-08-28 12:08:06 -0400

    Refactored - included more doc blocks and respositioned the calling of functions to reflect document flow

--- blackjack.js
+++ blackjack.js
@@ -4,15 +4,27 @@
     var display = document.getElementById('cards');
     var cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
+    /* Display cards on page load*/
+    newCards();
+    /* Do not hit or stand on first two cards on page load */
+    checkResult(false, false);
 
+    /**
+     * Choose random card from cards array
+     * @return {String}     Value of the card
+     */
     function randomCard() {
         var card = Math.floor(Math.random() * cards.length);
         return cards[card];
     }
 
-    display.innerHTML = randomCard() + ' ' + randomCard();
-
-    checkResult(false, false);
+    /**
+     * Display two random cards
+     * @return {Void}
+     */
+    function newCards() {
+        display.innerHTML = randomCard() + ' ' + randomCard();
+    };
 
     /**
      * Check the result of the current cards and alert the game result
@@ -26,37 +38,42 @@
 
         var cardValue = 0;
 
-        var i, card;
-        for (i=0; i<cards.length; i++) {
-            card = cards[i];    
+        cards.forEach(function (card, i) {
             if ( Number(card) ) {
                 cardValue += Number(card);
-            } else if (card === 'J' || card == 'Q' || card === 'K') {
+            }
+
+            if (card === 'J' || card === 'Q' || card === 'K') {
                 cardValue += 10;
-            } else {
+            }
+
+            if (card === 'A') {
                 cardValue += 11;
             }
         });
 
         console.log(cardValue);
 
-
         if ( cardValue < 16 && standing ) {
             alert('Dealer wins.');
+            newCards();
         } else if ( cardValue <= 18 && standing ) {
             alert('Push!');
-        } else if ( (cardValue > 18 && standing ) || cardValue === 21) {
+            newCards();
+        } else if ( ( cardValue > 18 && standing ) || cardValue === 21) {
             alert('You win!');
+            newCards();
         } else if (cardValue > 21) {
             alert('You Bust.');
+            newCards();
         }
 
-        cardValue = 0;
-
-        display.innerHTML = randomCard();
-        display.innerHTML = display.innerHTML + ' ' + randomCard();
     }
 
+    /**
+     * Display a new card and check result
+     * @return {void}
+     */
     function hit() {
         display.innerHTML = display.innerHTML + ' ' + randomCard();
         checkResult(false, true);
@@ -67,9 +84,7 @@
     });
 
     document.getElementById('hit').addEventListener('click',function() {
-        hit ();
+        hit();
     });
-
-
 
 })();
